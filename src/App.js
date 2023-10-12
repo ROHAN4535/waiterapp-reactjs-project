72f884399e4bb83b0c2d05e55cbb95060c104ea7
ROHAN4535 <grohan468@gmail.com>
2023-10-12 20:36:09 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import FormInput from "./component/FormInput";
+import TableList from "./component/TableList";
 
 function App() {
+  const [tableList, setTableList] = useState([]);
+  const formSubmitHanlder = (tableData) => {
+  
+    setTableList((prevTableList) => {
+     
+      return [
+        ...prevTableList,
+        {id:tableData.id, price:tableData.price, dish:tableData.dish, table:tableData.table}
+      ]
+      
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h2>Welcome to our Restaurant</h2>
+      <FormInput onFormSubmit={formSubmitHanlder}/>
+      <TableList tables={tableList} data={tableList}/>
+    </Fragment>
   );
 }
 
---
+++ src/component/TableList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TableList = (props) => {
+  return (
+    <ul>
+      {props.tables.map((item) => {
+        return (
+
+          <li key={item.id}>
+            {`ID: ${item.id} - Price: ${item.price} - Dish: ${item.dish} - Table: ${item.table}`}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TableList;
